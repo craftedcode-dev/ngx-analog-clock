527776a0b00f00b66fd75b930d12e8674675de33
Aleksandar Cobovic <aleksandarkg93@gmail.com>
2025-12-05 21:36:12 +0100

    fix: improve mobile view layout for demo pages

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -8,16 +8,21 @@ import { filter } from 'rxjs/operators';
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class AppComponent implements OnInit {
+
+  isMenuOpen = false;
+
   constructor(private router: Router) {}
 
   ngOnInit() {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      window.scrollTo(0, 0);
-    });
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo(0, 0);
+
+        this.isMenuOpen = false;
+      });
   }
 }
